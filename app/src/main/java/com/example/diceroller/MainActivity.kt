74f4b20ed6c961b2187e6d21bfc99b6b6a9050ad
thedarkarchitect <romanreynolds240@gmail.com>
2023-05-11 17:07:39 +0300

    fourth commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,14 +3,25 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)//this inflates the activity main to show the view
 
-        val rollButton : Button = findViewById(R.id.roll_button)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
 
-        rollButton.text = "Let's roll"
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        val randomInt = Random.nextInt(6) + 1//this is going to let us get a random num between 1 and 6
+
+        resultText.text = randomInt.toString()
     }
 }
