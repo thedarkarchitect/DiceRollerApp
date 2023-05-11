468ad9c02c4a6120ddd8cc228d33fbfbe728ba7b
thedarkarchitect <romanreynolds240@gmail.com>
2023-05-11 15:39:27 +0300

    third commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)//this inflates the activity main to show the view
+
+        val rollButton : Button = findViewById(R.id.roll_button)
+
+        rollButton.text = "Let's roll"
     }
 }
