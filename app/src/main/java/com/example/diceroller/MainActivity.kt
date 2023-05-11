7b6b2b17b5815116c63aeea6321915ffbf860b25
thedarkarchitect <romanreynolds240@gmail.com>
2023-05-11 17:39:47 +0300

    fifth commit

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    lateinit var diceImage: ImageView //promise to initialize when needed
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)//this inflates the activity main to show the view
 
+        diceImage = findViewById(R.id.dice_image)
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener {
             rollDice()
@@ -18,10 +21,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
+        val drawableResource = when (Random.nextInt(6) + 1){//this is going to let us get a random num between 1 and 6
+            //this will hold the dice at that particular randomInt moment as a resource id
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
-        val randomInt = Random.nextInt(6) + 1//this is going to let us get a random num between 1 and 6
+        diceImage.setImageResource(drawableResource)//then we will pass the resource id into the setImageResource
 
-        resultText.text = randomInt.toString()
     }
 }
